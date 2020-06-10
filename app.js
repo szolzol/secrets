7f8d49f7874bcc3460ebf269fa2a38973ba8b92c
szolzol <szolzol@users.noreply.github.com>
2020-06-10 23:57:29 +0200

    Initial commit!

---
+++ app.js
@@ -0,0 +1,74 @@
+//jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose')
+
+const app = express()
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema)
+
+app.get('/', function (req, res) {
+    res.render('home')
+});
+app.get('/login', function (req, res) {
+    res.render('login')
+});
+app.get('/register', function (req, res) {
+    res.render('register')
+});
+
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save(function (err) {
+        if (!err) {
+            res.render("secrets")
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUser) {
+        if (!err) {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets')
+                }
+            }
+        } else {
+            console.log(err);
+            
+        }
+    })
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, function () {
+    console.log(`Example app listening on port port!`)
+})
