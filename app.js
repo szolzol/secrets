ca29a4931f591cc8ce3981d9045c632b5975755e
szolzol <szolzol@users.noreply.github.com>
2020-06-11 23:22:27 +0200

    add env var

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //jshint esversion:6
-
+require("dotenv").config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption")
 
 const app = express()
 
@@ -18,10 +19,13 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
     useUnifiedTopology: true
 });
 
-const userSchema = {
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
-}
+})
+
+const secret = process.env.secret
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]})
 
 const User = new mongoose.model("User", userSchema)
 
